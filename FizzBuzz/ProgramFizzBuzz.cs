6d5cb7394db364d1e7001c0c0a7c265392cc54a7
Khanchai <khanchai.c@ssd.co.th>
2013-07-19 17:19:25 +0700

    Signed-off-by: Khanchai <khanchai.c@ssd.co.th>

---
+++ FizzBuzz/ProgramFizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class ProgramFizzBuzz
+    {
+        static void Main(string[] args)
+        {
+
+            	for(int i=1; i<=100; i++)
+	            {
+                    if (i % 3 == 0)
+                    {
+                        System.Console.WriteLine("Fizz");
+                    }
+                    if (i % 5 == 0)
+                    {
+                        System.Console.WriteLine("Buzz");
+                    }
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        System.Console.WriteLine("Fizz Buzz");
+                    }
+                    if (i % 3 != 0 && i % 5 != 0)
+                    {
+                        System.Console.WriteLine(i);
+                    }
+	            }
+                System.Console.ReadKey();
+        }
+    }
+}

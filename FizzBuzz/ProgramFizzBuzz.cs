e1347290a4db0178139da725471eb5c02972dde0
Khanchai <khanchai.c@ssd.co.th>
2013-07-24 15:00:53 +0700

    Update ProgramFizzBuzz.cs
    edit by khun

--- FizzBuzz/ProgramFizzBuzz.cs
+++ FizzBuzz/ProgramFizzBuzz.cs
@@ -13,19 +13,19 @@ namespace FizzBuzz
 
             	for(int i=1; i<=100; i++)
 	            {
-                    if (i % 3 == 0)
+                    else if (i % 3 == 0)
                     {
                         System.Console.WriteLine("Fizz");
                     }
-                    if (i % 5 == 0)
+                    else if (i % 5 == 0)
                     {
                         System.Console.WriteLine("Buzz");
                     }
-                    if (i % 3 == 0 && i % 5 == 0)
+                    else if (i % 3 == 0 && i % 5 == 0)
                     {
                         System.Console.WriteLine("Fizz Buzz");
                     }
-                    if (i % 3 != 0 && i % 5 != 0)
+                    else if (i % 3 != 0 && i % 5 != 0)
                     {
                         System.Console.WriteLine(i);
                     }
